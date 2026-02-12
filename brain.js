20c14e58fe840698b8ae9350d46dde67a74893d9
Ruchika <ruchikasinha229@gmail.com>
2026-02-12 13:11:23 +0530

    update wallet

--- brain.js
+++ brain.js
@@ -17,7 +17,7 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     role: { type: String, default: 'student' }, 
-    wallet: { type: Number, default: 1000 },
+    wallet: { type: Number, default: 0 },
     purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
 });
 
